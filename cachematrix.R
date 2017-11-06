ecc677d548f0619a0b697f47f7eba217b3b7becb
Marcos Martinez <marcos.martinez.romero@gmail.com>
2017-11-06 00:17:13 -0500

    finish programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
+## Matrix inversion is usually a costly computation and there may be 
+## some benefit to caching the inverse of a matrix rather than compute 
+## it repeatedly (there are also alternatives to matrix inversion that 
+## we will not discuss here). Your assignment is to write a pair of 
+## functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function creates a "matrix" object that caches the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  matrix <- x
+  setMatrix <- function(y) {
+    matrix <<- y
+    inverse <<- NULL
+  }
+  getMatrix <- function() matrix
+  setInverse <- function(im) {
+    inverse <<- im
+  }
+  getInverse <- function() inverse
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by the makeCacheMatrix function.
+## If the inverse has already been calculated (and the matrix has not changed), then cachesolve retrieves
+## the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Check if the matrix has not changed and also if the inverse has not been calculated already
+  if (exists("cacheMatrix") && x == cacheMatrix$getMatrix() && !is.null(cacheMatrix$getInverse())) {
+      message("getting cached inverse matrix")
+      return(cacheMatrix$getInverse())
+  }
+  # Cache matrix and its inverse
+  message("caching inverse matrix")
+  cacheMatrix <<- makeCacheMatrix(x)
+  cacheMatrix$setInverse(solve(x))
+  cacheMatrix$getInverse()
 }
+
+## Sample executions:
+
+## First execution, the cache is empty so the inverse is cached
+# > m = matrix(c(2,5,1,3), nrow=2, ncol=2)
+# > cacheSolve(m)
+# caching inverse matrix
+# [,1] [,2]
+# [1,]    3   -1
+# [2,]   -5    2
+
+## Second execution, retrieve the inverse from the cache
+# > cacheSolve(m)
+# getting cached inverse matrix
+# [,1] [,2]
+# [1,]    3   -1
+# [2,]   -5    2
+
+## Third execution, we pass a different matrix. The cache is regenerated
+# > m2 = matrix(c(4,1,2,5), nrow=2, ncol=2)
+# > cacheSolve(m2)
+# caching inverse matrix
+# [,1]       [,2]
+# [1,]  0.27777778 -0.1111111
+# [2,] -0.05555556  0.2222222
+
